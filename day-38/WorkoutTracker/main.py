2c335718f3277d7bdcae54886d05fa8c41591584
ifefrost <temiladeladipo@hotmail.com>
2024-02-01 00:08:36 -0500

    completed day 38

--- day-38/WorkoutTracker/main.py
+++ day-38/WorkoutTracker/main.py
@@ -2,7 +2,8 @@ from config import *
 from datetime import datetime
 import requests
 
-# prompt = input("What exercises did you do today? ")
+prompt = input("What exercises did you do today? ")
+moment = datetime.now()
 
 nutritionix_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
@@ -11,29 +12,28 @@ header = {
     "x-app-key": API_KEY
 }
 
-# params = {
-#     "query": prompt
-# }
+params = {
+    "query": prompt
+}
 
-# response = requests.post(url=nutritionix_endpoint, headers=header, json=params)
-# workout = response.json()["exercises"][0]
-# print(workout)
+response = requests.post(url=nutritionix_endpoint, headers=header, json=params)
+workout = response.json()["exercises"][0]
+print(workout)
 
 sheety_post_endpoint = "https://api.sheety.co/c21512e9714ba8e73dd12b458b57354d/workoutTracking/workouts"
 sheety_get_endpoint = "https://api.sheety.co/c21512e9714ba8e73dd12b458b57354d/workoutTracking/workouts"
 
 
-# workout_params = {
-#     "workout": {
-#         "date": "21/07/2020",
-#         "time": "15:00:00",
-#         "exercise": workout['user_input'].title(),
-#         "duration": workout['duration_min'],
-#         "calories": workout['nf_calories']
-#     }
-# }
+workout_params = {
+    "workout": {
+        "date": moment.strftime("%d/%m/%Y"),
+        "time": moment.strftime("%H:%M:%S"),
+        "exercise": workout['user_input'].title(),
+        "duration": workout['duration_min'],
+        "calories": workout['nf_calories']
+    }
+}
 
-# response = requests.post(url=sheety_post_endpoint, json=workout_params)
-# print(response.text)
+response = requests.post(url=sheety_post_endpoint, json=workout_params)
+print(response.text)
 
-print()
